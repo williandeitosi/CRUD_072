cf75d6b8f0042730da4f650eb1390528ce317fff
williandeitosi <williandeitosi@gmail.com>
2025-06-30 19:29:39 -0300

    feat: add endpoint login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { confirmAccount, registerUser } from "../controllers/userController.js";
+import {
+  confirmAccount,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.get("/confirm", confirmAccount);
+router.post("/login", loginUser);
 
 export default router;
