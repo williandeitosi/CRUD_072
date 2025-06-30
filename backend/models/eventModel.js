e6433e55a9e4265219a5bb1e720da4bf6f560ee8
williandeitosi <williandeitosi@gmail.com>
2025-06-30 20:53:51 -0300

    fix: remove method duplicated

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -11,13 +11,13 @@ export async function createEvent(title, description, date, userId) {
 
 export async function getEventsByUser(userId) {
   const [rows] = await pool.query(
-    `SELECT * FROM events WHERE user_id = ? ORDER BY date ASC`,
+    `SELECT * FROM events WHERE user_id = ? ORDER BY id ASC`,
     [userId]
   );
   return rows;
 }
 
-export async function updateEvent(eventId, userId) {
+export async function updateEvent(title, description, date, eventId, userId) {
   const [result] = await pool.query(
     `UPDATE events SET title = ?, description = ?, date = ? WHERE id = ? AND user_id = ?`,
     [title, description, date, eventId, userId]
@@ -33,13 +33,4 @@ export async function deleteEvent(eventId, userId) {
   );
 
   return result.affectedRows > 0;
-}
-
-export async function getAllEventsByUser(userId) {
-  const [result] = await pool.query(
-    `SELECT * FROM events WHERE user_id = ? ORDER BY id ASC`,
-    [userId]
-  );
-
-  return result;
 }
