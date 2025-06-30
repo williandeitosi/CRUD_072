d30dcf355a2b5aa521fb97ae3686cd7cf87cabbb
williandeitosi <williandeitosi@gmail.com>
2025-06-30 19:29:50 -0300

    feat: create login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
 import {
   confirmUser,
   createUser,
@@ -58,5 +59,46 @@ export async function confirmAccount(req, res) {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Error confirming account" });
+  }
+}
+
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email && !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password is required" });
+    }
+
+    const user = await findUserByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (!user.confirmed) {
+      return res
+        .status(403)
+        .json({ message: "Please confirm your email before accessing" });
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Login error" });
   }
 }
