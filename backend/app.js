20b43e3c9751d374f7c5d6d68643a34f5d73f612
williandeitosi <williandeitosi@gmail.com>
2025-06-30 20:30:52 -0300

    feat: add routes events

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", userRoutes);
+app.use("/events", eventRoutes);
 
 app.get("/healthcheck", (req, res) => {
   res.status(200).json({ status: "ok" });
