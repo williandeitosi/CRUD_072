14fa794f194f189e436320802c4ceea1fe1f43c5
williandeitosi <williandeitosi@gmail.com>
2025-06-30 19:45:29 -0300

    feat: add get events by user

--- backend/middleware/eventModel.js
+++ backend/middleware/eventModel.js
@@ -1,11 +1,19 @@
 import { pool } from "../database/conecction";
 
 export async function createEvent(title, description, date, userId) {
-  const [result] = pool.query(
+  const [result] = await pool.query(
     `INSERT INTO events (title, description, date, userId) VALUES (?,?,?,?)`[
       (title, description, date, userId)
     ]
   );
 
   return result.insertId;
+}
+
+export async function getEventsByUser(userId) {
+  const [rows] = await pool.query(
+    `SELECT * FROM events WHERE user_id = ? ORDER BY date ASC`,
+    [userId]
+  );
+  return rows;
 }
