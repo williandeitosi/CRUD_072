04f857fe992b65a4a42cb75785ee8d31cb6d74d0
williandeitosi <williandeitosi@gmail.com>
2025-06-30 20:33:03 -0300

    feat: add endpoint to lista all events

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createEventHandler } from "../controllers/eventController.js";
+import {
+  createEventHandler,
+  listAllEventsByUser,
+} from "../controllers/eventController.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/", authenticateToken, createEventHandler);
+router.get("/all", authenticateToken, listAllEventsByUser);
 
 export default router;
