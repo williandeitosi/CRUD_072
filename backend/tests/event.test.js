2d140fcc9776711645d225faf344a47c71aaa2f5
williandeitosi <williandeitosi@gmail.com>
2025-07-03 20:41:31 -0300

    feat: add create event without title

--- backend/tests/event.test.js
+++ backend/tests/event.test.js
@@ -1,32 +1,37 @@
 import request from "supertest";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import app from "../app.js";
 import { pool } from "../database/conecction.js";
 import * as eventModel from "../models/eventModel.js";
 
-describe("event flow", () => {
-  let authToken;
-  beforeAll(async () => {
-    const email = `test${Date.now()}@test.com`;
-    const password = "123456";
+let authToken;
+beforeAll(async () => {
+  const email = `test${Date.now()}@test.com`;
+  const password = "123456";
 
-    await request(app).post("/users/register").send({ email, password });
+  await request(app).post("/users/register").send({ email, password });
 
-    const [rows] = await pool.query(
-      "SELECT confirmation_token FROM users WHERE email = ?",
-      [email]
-    );
-    const confirmationToken = rows[0].confirmation_token;
+  const [rows] = await pool.query(
+    "SELECT confirmation_token FROM users WHERE email = ?",
+    [email]
+  );
+  const confirmationToken = rows[0].confirmation_token;
 
-    await request(app).get(`/users/confirm?token=${confirmationToken}`);
+  await request(app).get(`/users/confirm?token=${confirmationToken}`);
 
-    const res = await request(app)
-      .post("/users/login")
-      .send({ email, password });
+  const res = await request(app).post("/users/login").send({ email, password });
 
-    authToken = res.body.token;
-  });
+  authToken = res.body.token;
+});
 
+afterAll(async () => {
+  await pool.query(
+    `DELETE FROM users
+      WHERE email LIKE 'test%@test.com'`
+  );
+  await pool.end();
+});
+describe("Event handlers", () => {
   it("should create an event successfully", async () => {
     vi.spyOn(eventModel, "createEvent").mockResolvedValue(123);
     const res = await request(app)
@@ -42,11 +47,13 @@ describe("event flow", () => {
     expect(res.body).toHaveProperty("eventId", 123);
   });
 
-  afterAll(async () => {
-    await pool.query(
-      `DELETE FROM users
-      WHERE email LIKE 'test%@test.com'`
-    );
-    await pool.end();
+  it("should fail to create event without title", async () => {
+    const res = await request(app)
+      .post("/events")
+      .set(`Authorization`, `Bearer ${authToken}`)
+      .send({});
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toMatch("Title is required");
   });
 });
