09eb4d766b7d0b0a4ca8aa6c4f909defdaf44764
williandeitosi <williandeitosi@gmail.com>
2025-07-03 19:56:03 -0300

    feat: add create event test

---
+++ backend/tests/event.test.js
@@ -0,0 +1,52 @@
+import request from "supertest";
+import { describe, it } from "vitest";
+import app from "../app.js";
+import { pool } from "../database/conecction.js";
+import * as eventModel from "../models/eventModel.js";
+
+describe("event flow", () => {
+  let authToken;
+  beforeAll(async () => {
+    const email = `test${Date.now()}@test.com`;
+    const password = "123456";
+
+    await request(app).post("/users/register").send({ email, password });
+
+    const [rows] = await pool.query(
+      "SELECT confirmation_token FROM users WHERE email = ?",
+      [email]
+    );
+    const confirmationToken = rows[0].confirmation_token;
+
+    await request(app).get(`/users/confirm?token=${confirmationToken}`);
+
+    const res = await request(app)
+      .post("/users/login")
+      .send({ email, password });
+
+    authToken = res.body.token;
+  });
+
+  it("should create an event successfully", async () => {
+    vi.spyOn(eventModel, "createEvent").mockResolvedValue(123);
+    const res = await request(app)
+      .post("/events")
+      .set("Authorization", `Bearer ${authToken}`)
+      .send({
+        title: "My Event",
+        description: "Test description",
+        date: "2025-12-31",
+      });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty("eventId", 123);
+  });
+
+  afterAll(async () => {
+    await pool.query(
+      `DELETE FROM users
+      WHERE email LIKE 'test%@test.com'`
+    );
+    await pool.end();
+  });
+});
