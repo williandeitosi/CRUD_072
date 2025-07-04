1f53a018fbec882fff868387b2d953927dd877db
williandeitosi <williandeitosi@gmail.com>
2025-07-03 22:50:09 -0300

    feat: initialized swagger

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import { setupSwagger } from "./swagger.js";
 
 const app = express();
 
 app.use(express.json());
+setupSwagger(app); // habilita /api-docs
 app.use("/users", userRoutes);
 app.use("/events", eventRoutes);
 
