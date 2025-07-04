56c7e17785ec0f683c7d88bd0d75587ee185717b
williandeitosi <williandeitosi@gmail.com>
2025-07-03 22:57:57 -0300

    feat: add hate limite

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { setupSwagger } from "./swagger.js";
 
 const app = express();
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+app.use(globalLimiter);
 
 app.use(express.json());
 setupSwagger(app); // habilita /api-docs
