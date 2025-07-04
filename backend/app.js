56813a785ce097003337c09c694083dcf1e92b53
williandeitosi <williandeitosi@gmail.com>
2025-07-04 09:25:02 -0300

    feat: connect frontend in backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
+import path from "path";
+import { fileURLToPath } from "url";
 import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { setupSwagger } from "./swagger.js";
@@ -17,15 +19,23 @@ const globalLimiter = rateLimit({
   },
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(globalLimiter);
 
 app.use(express.json());
-setupSwagger(app); // habilita /api-docs
+app.use(express.static(path.join(__dirname, "../frontend")));
+setupSwagger(app);
 app.use("/users", userRoutes);
 app.use("/events", eventRoutes);
 
 app.get("/healthcheck", (req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
 export default app;
